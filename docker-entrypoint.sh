5d0164fcaa59dfeefcfdef1a0e8c15381c4cc1d8
Dimitrios Desyllas (pc_magas) <you@example.com>
2016-12-28 16:14:42 +0200

    More Robustness

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ ! -e piwik.php ]; then
-	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
-	chown -R www-data .
-fi
+# if [ ! -e piwik.php ]; then
+# 	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
+# 	chown -R www-data .
+# fi
+
+cp -R /usr/src/piwik/* /var/www/html
+chown -R www-data:www-data .
 
 exec "$@"
